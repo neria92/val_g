219e0ca892adb912f1d7d774cf4fe46c21cd17cb
Manuel Neria <m.neria@ciencias.unam.mx>
2020-05-26 19:09:08 -0500

    solo una prueba

--- main_parallel.py
+++ main_parallel.py
@@ -1,4 +1,4 @@
-#Manuel Neria #Push #testGCP
+#Manuel Neria #Push #NovatestGCP
 from imageai.Prediction.Custom import CustomImagePrediction
 from imutils.object_detection import non_max_suppression
 from tensorflow.keras.preprocessing import image as ig
